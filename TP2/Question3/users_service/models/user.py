17282c08cde298ea410ec32d12c5a1ef51356442
antoine-granger <antoine77.granger@outlook.fr>
2023-11-07 20:16:11 -0500

    [UPD] add login but no finish yet

--- TP2/Question3/users_service/models/user.py
+++ TP2/Question3/users_service/models/user.py
@@ -1,13 +1,32 @@
 from users_service import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80),  nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(80), nullable=False)
+
+    def set_password(self, password):
+        """
+        Creates a hashed password and stores it in the password_hash field.
+
+        :param password: The user's password.
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """
+        Checks the hashed password against the given password.
+
+        :param password: The password to check against the hashed password.
+        :return: True if the password matches, False otherwise.
+        :rtype: bool
+        """
+        return check_password_hash(self.password_hash, password)
 
     def serialize(self):
         """
@@ -18,7 +37,6 @@ class User(db.Model):
         """
         return {
             'id': self.id,
-            'username': self.title,
-            'password': self.author,
+            'username': self.username,
             'role': self.role
         }
--- TP2/Question3/users_service/views/user_views.py
+++ TP2/Question3/users_service/views/user_views.py
@@ -2,25 +2,43 @@ from users_service import app, db
 from users_service.models.user import User
 from users_service.models.available_ids import AvailableIDs
 from flask import jsonify, request
+from werkzeug.security import check_password_hash
 
 
 def get_min_available_id():
-    auth_ids = db.session.query(User.id).all()
-    auth_ids = [auth_id[0] for auth_id in auth_ids]
-    if not auth_ids:
+    user_ids = db.session.query(User.id).all()
+    user_ids = [user_id[0] for user_id in user_ids]
+    if not user_ids:
         return 1
     # Trouver le plus petit ID non utilisé
     # Pour cela, on crée une série d'IDs à partir de 1 jusqu'au maximum ID utilisé + 1
-    all_ids = set(range(1, max(auth_ids) + 1))
-    available_ids = all_ids - set(auth_ids)
+    all_ids = set(range(1, max(user_ids) + 1))
+    available_ids = all_ids - set(user_ids)
 
     # Retourner le plus petit ID disponible
-    return min(available_ids) if available_ids else max(auth_ids) + 1
+    return min(available_ids) if available_ids else max(user_ids) + 1
 
 
 def remove_available_id(used_id):
     AvailableIDs.query.filter_by(id=used_id).delete()
     db.session.commit()
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    # Obtenir les données d'authentification
+    username = request.json["username"]
+    password = request.json["password"]
+
+    # Rechercher l'utilisateur dans la DB
+    user = User.query.filter_by(username=username).first()
+
+    # Vérifier le mot de passe
+    if user and check_password_hash(user.password_hash, password):
+        # Retourner les données utilisateur nécessaires pour le token
+        return jsonify({"username": user.username, "role": user.role}), 200
+    else:
+        return jsonify({"message": "Invalid username or password"}), 401
 
 
 @app.route("/users", methods=["GET"])
@@ -44,8 +62,15 @@ def delete_user(user_id):
 def post_user():
     username = request.json["username"]
     password = request.json["password"]
+    role = request.json["role"]
+    # Check if the username already exists
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        # If a user with this username already exists, return a conflict response
+        return jsonify({"message": "Username already taken"}), 409
     min_id = get_min_available_id()
-    user = User(id=min_id, username=username, title=password)
+    user = User(id=min_id, username=username, role=role)
+    user.set_password(password)
     db.session.add(user)
     db.session.commit()
     return jsonify(user.serialize()), 201
@@ -56,8 +81,9 @@ def update_user(user_id):
     user = User.query.get(user_id)
     if user is None:
         return jsonify({"message": "user not found"}), 404
-    user.title = request.json["username"]
-    user.author = request.json["password"]
+    user.username = request.json["username"]
+    user.set_password(request.json["password"])
+    user.role = request.json["role"]
     db.session.commit()
     return jsonify({"message": "user updated successfully"}), 200
 
