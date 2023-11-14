f7f0905c76985fff99e7c79fa3dfd6d3f26050f0
antoine-granger <antoine77.granger@outlook.fr>
2023-11-14 00:58:36 -0500

    [UPD] add sleep for first connection to DB and postman request file

--- TP2/Question3/users_service/run.py
+++ TP2/Question3/users_service/run.py
@@ -1,9 +1,12 @@
+from time import sleep
+
 from users_service import app, db
 from users_service.models.available_ids import AvailableIDs
 from users_service.models.user import User
 
 
 if __name__ == '__main__':
+    sleep(60)
     with app.app_context():
         db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
